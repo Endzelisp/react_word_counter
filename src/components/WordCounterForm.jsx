a6755c1d3f06ef3d8ce532d961b29947e2cab445
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-03-02 19:16:30 -0400

    Add structure to the component and wire up the logic

--- src/components/WordCounterForm.jsx
+++ src/components/WordCounterForm.jsx
@@ -1,13 +1,33 @@
-import styled from 'styled-components'
+import { useState } from 'react';
+import styled from 'styled-components';
+import Textarea from './Textarea';
+import ControlButton from './ControlButton';
+import { sanitizeText } from '../utils';
+import { wordCounter } from '../utils';
 
-function WordCounterForm({children}) {
+function WordCounterForm() {
+
+  const [text, setText] = useState('');
 
   function submitHandler(e) {
-    e.preventDefault()
+    e.preventDefault();
+    const sanitizedText = sanitizeText(text);
+    const wordsArray = wordCounter(sanitizedText);
+    console.log(wordsArray)
+  }
+
+  function resetHandler() {
+    setText('')
   }
 
   return (
-    <Form onSubmit={submitHandler}>{children}</Form>
+    <Form onSubmit={submitHandler} onReset={resetHandler}>
+      <Textarea text={text} setText={setText}/>
+      <ButtonsWrapper>
+          <ControlButton type={'submit'}>Count</ControlButton>
+          <ControlButton type={'reset'}>Reset</ControlButton>
+        </ButtonsWrapper>
+    </Form>
   )
 }
 
@@ -16,6 +36,11 @@ const Form = styled.form`
   flex-direction: column;
   align-items: center;
 `;
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+`
 
 export default WordCounterForm;
 
