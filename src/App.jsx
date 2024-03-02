36c437246b3b2f3ee6ceea2dd3f790914ea972cc
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-03-02 12:44:00 -0400

    Change App to a function declaration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,28 @@
 import GlobalStyles from './GlobalStyles';
+import styled from 'styled-components';
+import Header from './components/Header';
+import WordCounterForm from './components/WordCounterForm';
+import Textarea from './components/Textarea';
+import ControlButton from './components/ControlButton';
 
-
-export const App = () => {
+function App() {
   return (
     <>
+      <Header>Word Counter</Header>
+      <WordCounterForm>
+        <Textarea />
+        <ButtonsWrapper>
+          <ControlButton type={'submit'}>Count</ControlButton>
+          <ControlButton type={'reset'}>Reset</ControlButton>
+        </ButtonsWrapper>
+      </WordCounterForm>
       <GlobalStyles />
     </>
   );
 };
+
+const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 16px;
+`
+export default App
