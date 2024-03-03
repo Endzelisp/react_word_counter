01b6ffa19eb30beb798a0d7ba362d6a5801cced4
Armando Endzelis <98135117+Endzelisp@users.noreply.github.com>
2024-03-03 19:13:56 -0400

    Improve styling

--- src/components/WordCountRenderer.jsx
+++ src/components/WordCountRenderer.jsx
@@ -1,8 +1,14 @@
 import styled from "styled-components";
+import close from "../assets/close.png"
 
 function WordCountRenderer({frequency, children}) {
   return (
-    <p><Count>{frequency}</Count>{children}</p>
+    <Wrapper>
+      <p>{children} <Count>{frequency}</Count></p>
+      <DeleteBtn>
+        <img src={close} alt="close button" />
+      </DeleteBtn>
+    </Wrapper>
   )
 }
 
@@ -11,5 +17,26 @@ const Count = styled.span`
   font-size: 1rem;
   padding-inline-end: 16px;
 `
+
+const DeleteBtn = styled.button`
+  appearance: none;
+  border: none;
+  background-color: transparent;
+  border-radius: 3px;
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 4px 8px;
+  max-width: 300px;
+  border: 1px solid hsl(0, 0%, 50%);
+  border-radius: 5px;
+
+  &:hover {
+    background-color: hsl(0, 0%, 90%);
+    border: 1px solid hsl(0, 0%, 70%);
+  }
+`;
 
 export default WordCountRenderer;
